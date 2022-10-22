e6d564423b27043c167d9f2740b7b98ebecfce87
HoangAnh <mhoanganh25@gmail.com>
2022-10-22 12:46:40 +0700

    basic function channel and message

--- service/channel/controller/controller.go
+++ service/channel/controller/controller.go
@@ -15,6 +15,21 @@ type CreateNewChannel struct {
 	Name string `json:"name"`
 }
 
+type AddNewMember struct {
+	Name       string `json:"name"`
+	MemberId   int    `json:"memberId"`
+	MemberName string `json:"memberName"`
+}
+
+type DeleteAChannel struct {
+	Name string `json:"name"`
+}
+
+type ChangeChannelName struct {
+	Name string `json:"name"`
+	Id   int    `json:"id"`
+}
+
 func CreateChannel(w http.ResponseWriter, r *http.Request) {
 
 	//Get Parameters JWT claims header
@@ -37,7 +52,6 @@ func CreateChannel(w http.ResponseWriter, r *http.Request) {
 	user := &usermodel.User{
 		Id: payload.UserId,
 	}
-	_ = json.NewDecoder(r.Body).Decode(&user)
 	err = user.GetUserById()
 	if err != nil {
 		router.ResponseInternalError(w, err.Error())
@@ -45,17 +59,22 @@ func CreateChannel(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	NewChannel := &CreateNewChannel{}
-	_ = json.NewDecoder(r.Body).Decode(NewChannel)
+	newChannel := &CreateNewChannel{}
+	err = json.NewDecoder(r.Body).Decode(newChannel)
+	if err != nil {
+		log.Println(log.LogLevelDebug, "Name null", err)
+		router.ResponseInternalError(w, err.Error())
+		return
+	}
 
 	var channel = &model.Channel{
-		Name:       NewChannel.Name,
+		Name:       newChannel.Name,
 		MemberId:   user.Id,
 		MemberName: user.Name,
 		Role:       constant.CHANNEL_ROLE_HOST,
 	}
-
 	err = channel.CreateChannel()
+
 	if err != nil {
 		log.Println(log.LogLevelDebug, "CreateChannel", err)
 		router.ResponseInternalError(w, err.Error())
@@ -63,4 +82,145 @@ func CreateChannel(w http.ResponseWriter, r *http.Request) {
 	}
 
 	router.ResponseSuccess(w, "B.ACC.201.C1", "Create channel successfully !!!")
+}
+
+func DeleteChannel(w http.ResponseWriter, r *http.Request) {
+
+	//Get Parameters JWT claims header
+	claims, err := auth.GetJWTClaims(r.Header.Get("X-JWT-Claims"))
+	if err != nil {
+		router.ResponseInternalError(w, err.Error())
+		log.Println(log.LogLevelDebug, "DeleteChannel: GetJWTClaims", err)
+		return
+	}
+
+	// Get payload data from claims
+	var payload = &auth.Payload{}
+	err = payload.GetDataFromClaims(claims)
+	if err != nil {
+		log.Println(log.LogLevelDebug, "DeleteChannel: GetDataFromClaims", err)
+		router.ResponseInternalError(w, err.Error())
+		return
+	}
+
+	user := &usermodel.User{
+		Id: payload.UserId,
+	}
+	err = user.GetUserById()
+	if err != nil {
+		router.ResponseInternalError(w, err.Error())
+		log.Println(log.LogLevelDebug, "DeleteChannel: Get users Database", err)
+		return
+	}
+
+	aChannel := &DeleteAChannel{}
+	err = json.NewDecoder(r.Body).Decode(aChannel)
+	if err != nil {
+		log.Println(log.LogLevelDebug, "Name null", err)
+		router.ResponseInternalError(w, err.Error())
+		return
+	}
+
+	var channel = &model.Channel{
+		Name:       aChannel.Name,
+		MemberName: user.Name,
+		Role:       constant.CHANNEL_ROLE_HOST,
+	}
+	err = channel.DeleteChannel()
+
+	if err != nil {
+		log.Println(log.LogLevelDebug, "Delete Channel", err)
+		router.ResponseInternalError(w, err.Error())
+		return
+	}
+
+	router.ResponseSuccess(w, "B.ACC.201.C1", "Delete channel successfully !!!")
+}
+
+func AddNewMemberToChannel(w http.ResponseWriter, r *http.Request) {
+
+	newMember := &AddNewMember{}
+	_ = json.NewDecoder(r.Body).Decode(newMember)
+
+	user := &usermodel.User{
+		Id: newMember.MemberId,
+	}
+	err := user.GetUserById()
+
+	if err != nil {
+		log.Println(log.LogLevelDebug, "Get user by id", err)
+		router.ResponseInternalError(w, err.Error())
+		return
+	}
+
+	var channel = &model.Channel{
+		Name:       newMember.Name,
+		MemberId:   user.Id,
+		MemberName: user.Name,
+		Role:       constant.CHANNEL_ROLE_MEMBER,
+	}
+
+	err = channel.AddMemberToChannel()
+
+	if err != nil {
+		log.Println(log.LogLevelDebug, "Add new member to channel", err)
+		router.ResponseInternalError(w, err.Error())
+		return
+	}
+
+	router.ResponseSuccess(w, "B.ACC.201.C1", "Add new member to channel successfully !!!")
+}
+
+func ChangeNameChannel(w http.ResponseWriter, r *http.Request) {
+
+	//Get Parameters JWT claims header
+	claims, err := auth.GetJWTClaims(r.Header.Get("X-JWT-Claims"))
+	if err != nil {
+		router.ResponseInternalError(w, err.Error())
+		log.Println(log.LogLevelDebug, "ChangeNameChannel: GetJWTClaims", err)
+		return
+	}
+
+	// Get payload data from claims
+	var payload = &auth.Payload{}
+	err = payload.GetDataFromClaims(claims)
+	if err != nil {
+		log.Println(log.LogLevelDebug, "ChangeNameChannel: GetDataFromClaims", err)
+		router.ResponseInternalError(w, err.Error())
+		return
+	}
+
+	user := &usermodel.User{
+		Id: payload.UserId,
+	}
+	err = user.GetUserById()
+	if err != nil {
+		router.ResponseInternalError(w, err.Error())
+		log.Println(log.LogLevelDebug, "ChangeNameChannel: Get users Database", err)
+		return
+	}
+
+	aChannel := &ChangeChannelName{}
+	err = json.NewDecoder(r.Body).Decode(aChannel)
+	if err != nil {
+		log.Println(log.LogLevelDebug, "Name null", err)
+		router.ResponseInternalError(w, err.Error())
+		return
+	}
+
+	var channel = &model.Channel{
+		Name:       aChannel.Name,
+		Id:         aChannel.Id,
+		MemberName: user.Name,
+		Role:       constant.CHANNEL_ROLE_HOST,
+	}
+	err = channel.ChangeNameChannel()
+
+	if err != nil {
+		log.Println(log.LogLevelDebug, "Change Name Channel", err)
+		router.ResponseInternalError(w, err.Error())
+		return
+	}
+
+	router.ResponseSuccess(w, "B.ACC.201.C1", "Change name channel successfully !!!")
 }
--- service/channel/model/channel.go
+++ service/channel/model/channel.go
@@ -12,6 +12,7 @@ type Channel struct {
 	MemberName  string `json:"memberName"`
 	CreatedDate string `json:"createdDate"`
 	Role        int    `json:"role"`
+	//Messages    []model.Message
 }
 
 // Insert new message to db
@@ -21,8 +22,29 @@ func (channel *Channel) CreateChannel() error {
 	return err
 }
 
+func (channel *Channel) AddMemberToChannel() error {
+	query := `INSERT INTO channels (name, member_id, member_name, role, created_date) VALUES ($1, $2, $3, $4, $5) RETURNING id;`
+	err := db.PSQL.QueryRow(query, channel.Name, channel.MemberId, channel.MemberName, channel.Role, utils.Timestamp()).Scan(&channel.Id)
+	return err
+}
+
 func (channel *Channel) DeleteChannel() error {
-	query := `DELETE FROM channels where id = $1;`
-	_, err := db.PSQL.Exec(query, channel.Id)
+	query := `DELETE FROM channels where name = $1 and member_name = $2 and role = $3;`
+	_, err := db.PSQL.Exec(query, channel.Name, channel.MemberName, channel.Role)
+	return err
+}
+
+func (channel *Channel) GetAllChannelsOfHost() error {
+	query := `SELECT * FROM channels where member_id = $1 and role = $2;`
+	err := db.PSQL.QueryRow(query, channel.MemberId, channel.Role).Scan(&channel.Id, &channel.Name, &channel.MemberId, &channel.MemberName, &channel.CreatedDate, &channel.Role)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (channel *Channel) ChangeNameChannel() error {
+	query := `UPDATE channels SET name = $1 WHERE id= $2 and member_name = $3 and role = $4`
+	_, err := db.PSQL.Exec(query, channel.Name, channel.Id, channel.MemberName, channel.Role)
 	return err
 }
