a25d49a0d97993e9e883160656827fe54346734e
vinhtc27 <vinhtc27@gmail.com>
2022-12-08 10:30:07 +0700

    done

--- service/static/sub_route.go
+++ service/static/sub_route.go
@@ -2,35 +2,19 @@ package static
 
 import (
 	"net/http"
-	"path"
 	"web-service/pkg/server"
 
 	"github.com/go-chi/chi"
 )
 
 var fePath = server.Config.GetString("FE_PATH")
+var fileServer = http.FileServer(http.Dir(fePath))
 
 var StaticSubRoute = chi.NewRouter()
 
 // Init package with sub-router for mails service
 func init() {
 	StaticSubRoute.Group(func(_ chi.Router) {
-		StaticSubRoute.Get("/html", ServeHTML)
-		StaticSubRoute.Get("/css/*", ServeCSS)
-		StaticSubRoute.Get("/js/*", ServeJS)
+		StaticSubRoute.Handle("/*", fileServer)
 	})
-}
-
-func ServeHTML(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, fePath+"/index.html")
-}
-
-func ServeCSS(w http.ResponseWriter, r *http.Request) {
-	cssFileName := path.Base(r.URL.Path)
-	http.ServeFile(w, r, fePath+"/static/css/"+cssFileName)
-}
-
-func ServeJS(w http.ResponseWriter, r *http.Request) {
-	jsFileName := path.Base(r.URL.Path)
-	http.ServeFile(w, r, fePath+"/static/js/"+jsFileName)
 }
